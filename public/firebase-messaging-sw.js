5848f70c2665bbde99a82a71d0d9acd4bbc88b0c
prajval-rai <prajvalrai2001@gmail.com>
2026-05-03 01:12:35 +0530

    FCM conf change

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,12 +3,12 @@ importScripts("https://www.gstatic.com/firebasejs/10.0.0/firebase-app-compat.js"
 importScripts("https://www.gstatic.com/firebasejs/10.0.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey:  "AIzaSyAl4_5r5wsNpt39edP0hBpIOkC2dn_OutA",
+  authDomain: "bartersystem-90a0b.firebaseapp.com",
+  projectId: "bartersystem-90a0b",
+  storageBucket: "bartersystem-90a0b.firebasestorage.app",
+  messagingSenderId: "311324772607",
+  appId:"1:311324772607:web:23ac44c3d34315cc03a4ca",
 });
 
 const messaging = firebase.messaging();
