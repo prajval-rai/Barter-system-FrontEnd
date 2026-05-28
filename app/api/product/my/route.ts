8e93faefa94327d429c8f38de9f844e038126d66
Prajval Rai <53143253+prajval-rai@users.noreply.github.com>
2026-05-28 16:57:08 +0530

    Update route.ts

--- app/api/product/my/route.ts
+++ app/api/product/my/route.ts
@@ -3,7 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 const BACKEND = process.env.NEXT_PUBLIC_BACKEND_URL!;
 
 export async function GET(req: NextRequest) {
-  console.log(`ccccccccccccccccccccccccccccccccccc${BACKEND}products/my_product`)
   const res = await fetch(`${BACKEND}products/my_product`, {
     headers: { cookie: req.headers.get("cookie") ?? "" },
   });
