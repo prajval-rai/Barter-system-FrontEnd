6c0a9ba69dc19d78bf49ebf502456eec01a1b9cc
Prajval Rai <53143253+prajval-rai@users.noreply.github.com>
2026-07-01 15:31:29 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from '@/components/Home/Hero/Hero';
 import WhyBuilt from '@/components/Home/WhyBuilt/WhyBuilt';
 import HowItWorks from '@/components/Home/HowItWorks/HowItWorks';
 import WhyExchange from '@/components/Home/WhyExchange/WhyExchange';
+import FAQ from '@/components/Home/FAQ/FAQ';
 import Stats from '@/components/Home/Stats/Stats';
 import CTA from '@/components/Home/CTA/CTA';
 import Footer from '@/components/Footer/Footer';
@@ -16,6 +17,7 @@ export default function Home() {
         <WhyBuilt />
         <HowItWorks />
         <WhyExchange />
+        <FAQ />
         <Stats />
         <CTA />
       </main>
