f5a7f234f3c6b2a8f78e69626b3e0851e2170a49
Prajval Rai <53143253+prajval-rai@users.noreply.github.com>
2026-07-29 16:06:58 +0530

    Update Hero.tsx

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -5,7 +5,7 @@ import LoginModal from "@/app/login/LoginModal";
 import { useAuth } from "@/context/AuthContext";
 import { useRouter } from "next/navigation";
 import Butterflies from "@/components/Butterfly/Butterfly";
-import ExchangeCard from "@/components/ExchangeCard/ExchangeCard";
+import ExchangeCard from "./ExchangeCard/ExchangeCard";
 
 export default function Hero() {
   const { user } = useAuth();
